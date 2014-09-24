b4153242b4124fb445beb9aec1e4bbdfb370a5e7
Phospher Lau <phospher@qq.com>
2014-09-24 15:53:08 +0800

    FIle Result output

--- src/command/CommandRun.py
+++ src/command/CommandRun.py
@@ -1,17 +1,30 @@
-import importlib
-from command.CommandParse import OutputType 
+import importlib, time, os
+from command.CommandParse import OutputType
 
 def _import_object(callable_object):
     module_name, obj_name = callable_object.rsplit('.', 1)
     module = importlib.import_module(module_name)
     return getattr(module, obj_name)
 
+def _generate_colsole_result(command_parse_result, command_result):
+    return {'type':'CONSOLE', 'result':command_result}
+
+def _generate_file_result(command_parse_result, command_resut):
+    file_name = '%s_%d' % (command_parse_result.output_name, (time.time() * 100))
+    file_path = os.path.join(os.getcwd(), 'DownloadFiles', file_name)
+    with open(file_path) as result_file:
+        result_file.write('>>')
+        result_file.write(input)
+        result_file.write('\n')
+        result_file.write(command_resut)
+    return {'type':'FILE', 'result':file_name}
+
 GENERATE_COMMAND_RESULT = {
-    OutputType.Console:None,
-    OutputType.File:None
+    OutputType.Console:_generate_colsole_result,
+    OutputType.File:_generate_file_result
 }
 
-def run_callable_object(callable_object, args, output_type):
+def run_callable_object(callable_object, args):
     try:
         obj = _import_object(callable_object)
         if callable(obj):
@@ -20,4 +33,7 @@ def run_callable_object(callable_object, args, output_type):
             command_result = 'Error-Invalid Command'
     except ImportError:
         command_result = 'Error-Invalid Command'
-    return GENERATE_COMMAND_RESULT[output_type](command_result)
+    return command_result
+
+def generate_command_result(command_parse_result, command_result):
+    return GENERATE_COMMAND_RESULT[command_parse_result.output_type](command_parse_result, command_result)
--- src/command/__init__.py
+++ src/command/__init__.py
@@ -1,3 +1,7 @@
+from command.CommandParse import parse_input
+from command.Configurations import CommandConfigurationManager
+from command import CommandRun
+
 class DynamicObject(object):
     def __init__(self, **kwargs):
         for key in kwargs:
@@ -5,3 +9,9 @@ class DynamicObject(object):
     
     def __setattr__(self, name, value):
         self.__dict__[name] = value
+
+def run_command(input, config_file_path):
+    command_parse = parse_input(input)
+    config_manager = CommandConfigurationManager(config_file_path)
+    command_result = CommandRun.run_callable_object(config_manager.get_callable_object(command_parse.command), command_parse.args)
+    return CommandRun.generate_command_result(command_parse, command_result)
